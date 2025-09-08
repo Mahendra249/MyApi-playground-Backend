cd42e8ce9c87bd91c19b4681886714f85717ff1b
Mahendra Sen <mahendrasen249@gmail.com>
2025-09-09 01:26:34 +0530

    updated

--- server.js
+++ server.js
@@ -1,17 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 import connectDB from "./config/db.js";
 import profileRoutes from "./routes/profileRoute.js";
 
 dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
+
 connectDB();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api", profileRoutes);
 
-app.get("/", (req, res) => res.send("welcome to my api"));
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`));
